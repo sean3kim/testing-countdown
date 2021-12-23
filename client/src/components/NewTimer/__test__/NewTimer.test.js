2463b818fbe4c086af913ffc27db07e02d6ff23a
sean kim <s3jkim@gmail.com>
2021-12-22 16:58:25 -0800

    added a test for reset button

--- client/src/components/NewTimer/__test__/NewTimer.test.js
+++ client/src/components/NewTimer/__test__/NewTimer.test.js
@@ -2,7 +2,6 @@ import { fireEvent, render, screen, act } from "@testing-library/react";
 import NewTimer from "../NewTimer";
 
 describe("timer functionality", () => {
-
     it("should have all the relevant components", () => {
         render(<NewTimer />);
         const startButton = screen.getByText(/start/i);
@@ -30,12 +29,37 @@ describe("timer functionality", () => {
 
     it("should decrease the second by 1 after a 1 second delay", async () => {
         render(<NewTimer />);
-        const secondField = screen.getByLabelText(/seconds/i);
         const button = screen.getByText(/start/i);
-        const timeText = screen.getByRole("heading");
+        //const timeText = screen.getByRole("heading");
+        const hourField = screen.getByLabelText(/hours/i);
+        const minuteField = screen.getByLabelText(/minutes/i);
+        const secondField = screen.getByLabelText(/seconds/i);
         fireEvent.change(secondField, { target: { value: 5 } });
         fireEvent.click(button)
         await act(async () => await new Promise((r) => setTimeout(r, 1000)));
-        expect(timeText.innerHTML).toBe("00:00:04")
+        //expect(timeText.innerHTML).toBe("00:00:04")
+        expect(hourField.value).toBe("0");
+        expect(minuteField.value).toBe("0");
+        expect(secondField.value).toBe("4");
+    })
+
+    it("should reset all the textfields to 0", async () => {
+        render(<NewTimer />);
+        const hourField = screen.getByLabelText(/hours/i);
+        const minuteField = screen.getByLabelText(/minutes/i);
+        const secondField = screen.getByLabelText(/seconds/i);
+        const startButton = screen.getByText(/start/i);
+        const resetButton = screen.getByText(/reset/i);
+
+        fireEvent.change(hourField, { target: { value: 5 } });
+        fireEvent.change(minuteField, { target: { value: 10 } });
+        fireEvent.change(secondField, { target: { value: 50 } });
+        fireEvent.click(startButton);
+        await act(async () => await new Promise((r) => setTimeout(r, 2000)));
+        fireEvent.click(resetButton);
+
+        expect(hourField.value).toBe("0");
+        expect(minuteField.value).toBe("0");
+        expect(secondField.value).toBe("0");
     })
 })
